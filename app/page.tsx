65b818d1d2c1dafaf12871b6b19e7f1ba94e46e4
Giorgi Jibladze <giorgi@omedia.ge>
2023-07-04 17:59:44 +0400

    started from prisma-postgres-auth-starter + mastodon basic auth works

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen bg-black">
+      <div className="w-screen h-screen flex flex-col justify-center items-center text-white">
+        Next Mastodon  
+      </div>
+    </div>
+  );
+}

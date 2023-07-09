0eb27e0700cfd21d5fcf382d093ea26ea5af09c9
Giorgi Jibladze <giorgi@omedia.ge>
2023-07-09 12:39:14 +0400

    Entire journey works (need to refactor and impl todos. refs #1

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,38 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import getMastodonConfig from "./mastodon-config";
+import { NextRequest, NextResponse } from "next/server";
+import url from "url";
+import { NextApiRequest, NextApiResponse } from "next";
 
-const handlerWrapper = async function (req: Request, res: Response) {
+const getActiveServerOrRefererHost = (req: NextApiRequest) => {
+  const activeServer = req.cookies.get("activeServer").value;
+
+  if (activeServer) {
+    return atob(activeServer);
+  }
+
+  const referer = req.headers.get("referer");
+  if (referer) {
+    const refererUrl = url.parse(referer);
+    return refererUrl.host;
+  }
+
+  return null;
+};
+
+const handlerWrapper = async function (
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const mastodonServer = getActiveServerOrRefererHost(req);
+
   const authOptions: NextAuthOptions = await getMastodonConfig(
-    "https://mastodon.social",
+    mastodonServer ?? "",
+    //todo: make this dynamic
     "http://localhost:3000",
   );
 
-  return NextAuth(authOptions);
+  return NextAuth(req, res, authOptions);
 };
 
 export { handlerWrapper as GET, handlerWrapper as POST };

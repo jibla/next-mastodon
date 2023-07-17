b79ab478340d69d1cf6ba3657d397ebaf0c99ec1
Giorgi Jibladze <giorgi@omedia.ge>
2023-07-17 10:22:55 +0400

    Add jest-fetch-mock. refs #1

--- jest.config.mjs
+++ jest.config.mjs
@@ -12,6 +12,7 @@ const config = {
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 
   testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async

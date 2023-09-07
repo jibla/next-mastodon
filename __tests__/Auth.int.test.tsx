091d0f08d9098802c6cb828e27ed0e1bf038e02b
Giorgi Jibladze <giorgi@omedia.ge>
2023-09-07 12:23:32 +0400

    Add int test for our getMastodonConfig() method. refs #30

--- __tests__/Auth.int.test.tsx
+++ __tests__/Auth.int.test.tsx
@@ -1,6 +1,7 @@
 import "@testing-library/jest-dom";
 import validateMastodonServer from "@/app/api/validate-server/validate-server";
 import OAuthCredentialsStorageService from "@/lib/auth";
+import getMastodonConfig from "@/app/api/auth/[...nextauth]/mastodon-config";
 
 fetchMock.disableMocks();
 
@@ -28,6 +29,19 @@ describe("Integration tests for authentication flow", () => {
     const creds = await storage.getCredentials(validAddress);
     expect(creds?.clientId).not.toBeNull();
     expect(creds?.clientSecret).not.toBeNull();
+  });
+
+  test("generate valid mastodon config for authjs", async () => {
+    const validAddress = "https://mastodon.social";
+    const callbackBaseUrl = "https://example.com";
+
+    const mastodonConfig = await getMastodonConfig(
+      validAddress,
+      callbackBaseUrl,
+    );
+    expect(mastodonConfig).not.toBeNull();
+    expect(mastodonConfig.providers[0].clientId).not.toBeNull();
+    expect(mastodonConfig.providers[0].clientSecret).not.toBeNull();
   });
 });
 

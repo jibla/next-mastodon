5265c38ee9adc8c7902ad3c8620da5d6e36ca105
Giorgi Jibladze <giorgi@omedia.ge>
2023-11-17 15:52:55 +0400

    Implement home, local and fededated feeds. Refactor feed port adapter. refs #44

--- lib/data/adapters/feed/mastojs/FeedsPortMastojsAdapter.ts
+++ lib/data/adapters/feed/mastojs/FeedsPortMastojsAdapter.ts
@@ -5,51 +5,49 @@ import FeedPort, {
 } from "@/lib/data/core/ports/FeedPort";
 import { injectable } from "inversify";
 import { MastojsClientFactory, fetchFeedPage } from "../../shared/mastojs";
+import { mastodon } from "masto";
 
 @injectable()
 export default class FeedsPortMastojsAdapter implements FeedPort {
   async getFeed(input: getFeedParams): Promise<getFeedOutput> {
     const client = await MastojsClientFactory.getClient();
 
-    let mastoJsRepo = client.v1.timelines.public;
-    let mastoJsListConf = {
-      local: false,
+    const config = {
+      local: input.type === feedTypes.local,
       limit: input.limit,
       maxId: input.startFrom
         ? (parseInt(input.startFrom) - 1).toString()
         : undefined,
     };
-    switch (input.type) {
-      case feedTypes.home:
-        mastoJsRepo = client.v1.timelines.home;
-        break;
-      case feedTypes.public:
-        mastoJsRepo = client.v1.timelines.public;
-        mastoJsListConf.local = false;
-        break;
-      case feedTypes.local:
-        mastoJsRepo = client.v1.timelines.public;
-        mastoJsListConf.local = true;
-        break;
-      default:
-        mastoJsRepo = client.v1.timelines.public;
-    }
 
-    const paginator = mastoJsRepo.list(mastoJsListConf);
+    const repo = this.getRepository(client, input.type);
 
+    const paginator = repo.list(config);
     const feed = await fetchFeedPage(paginator);
-    const nextFunc = async (): Promise<getFeedOutput> => {
-      const nextPageFeed = await fetchFeedPage(paginator);
 
+    const fetchNextPage = async (): Promise<getFeedOutput> => {
+      const nextPageFeed = await fetchFeedPage(paginator);
       return {
         feed: nextPageFeed,
-        next: nextFunc,
+        next: fetchNextPage,
       };
     };
 
     return {
       feed: feed,
-      next: nextFunc,
+      next: fetchNextPage,
     };
+  }
+
+  private getRepository(client: mastodon.rest.Client, type: feedTypes) {
+    switch (type) {
+      case feedTypes.home:
+        return client.v1.timelines.home;
+      case feedTypes.public:
+      case feedTypes.local:
+        return client.v1.timelines.public;
+      default:
+        throw new Error(`Unsupported feed type: ${type}`);
+    }
   }
 }

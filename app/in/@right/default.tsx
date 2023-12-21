dce45e0cf208952cd1061fe2ee5473fa2cc2a06a
Giorgi Jibladze <giorgi@omedia.ge>
2023-12-21 16:32:56 +0400

    Update default pane content. refs #63

--- app/in/@right/default.tsx
+++ app/in/@right/default.tsx
@@ -1,3 +1,12 @@
 export default function RightDefault() {
-  return <div>Tutorial How to use this client.</div>;
+  return (
+    <>
+      <h1 className="scroll-m-20 mt-10 text-center text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Next Mastodon
+      </h1>
+      <p className="leading-7 text-center [&:not(:first-child)]:mt-6">
+        Free and open-source Mastodon client for desktop use.
+      </p>
+    </>
+  );
 }

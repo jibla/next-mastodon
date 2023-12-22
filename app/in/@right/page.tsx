c8899b04cc5205569d13efe69d724d880e98dfc0
Giorgi Jibladze <giorgi@omedia.ge>
2023-12-22 15:12:52 +0400

    Update elephant. refs #63

--- app/in/@right/page.tsx
+++ app/in/@right/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Right() {
   return (
     <>
@@ -7,6 +9,14 @@ export default function Right() {
       <p className="leading-7 text-center [&:not(:first-child)]:mt-6">
         Free and open-source Mastodon client for desktop use.
       </p>
+
+      <Image
+        className="m-auto mt-3"
+        src="/homepage-image.png"
+        alt="Next mastodon image"
+        width={512}
+        height={512}
+      />
     </>
   );
 }

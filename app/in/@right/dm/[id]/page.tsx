62a4156ecc07da8698f61490c5d7a3a05ccadb60
Giorgi Jibladze <giorgi@omedia.ge>
2024-01-14 22:21:36 +0400

    Make conversations look like a normal chat. refs #64

--- app/in/@right/dm/[id]/page.tsx
+++ app/in/@right/dm/[id]/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import DmMessage from "@/components/direct-messages/DmMessage";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { Status } from "@/lib/data/core/entities/Status";
 import useConversatrion from "@/lib/hooks/useConversation";
+import { SendIcon } from "lucide-react";
 import { Key } from "react";
 
 export default function ConversationPage({
@@ -13,11 +16,23 @@ export default function ConversationPage({
   const { loading, conversation } = useConversatrion(params.id);
 
   return (
-    <>
+    <div className="flex flex-col">
       {!loading &&
         conversation.map((status: Status, index: Key | null | undefined) => (
-          <DmMessage key={index} status={status} />
+          <DmMessage
+            key={index}
+            status={status}
+            align={Number(index) % 2 === 0 ? "left" : "right"}
+          />
         ))}
-    </>
+      <div className="p-4 mt-2">
+        <form className="flex items-center space-x-2 flex-1">
+          <Input className="flex-1" placeholder="Type a message..." />
+          <Button type="submit">
+            <SendIcon className="h-5 w-5" />
+          </Button>
+        </form>
+      </div>
+    </div>
   );
 }
--- components/direct-messages/DmMessage.tsx
+++ components/direct-messages/DmMessage.tsx
@@ -2,64 +2,33 @@ import RelativeDate from "@/components/shared/relativeDate";
 import { Status } from "@/lib/data/core/entities/Status";
 import { AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import DOMPurify from "dompurify";
-import Bookmark from "../feed/actions/Bookmark";
-import Like from "../feed/actions/Like";
 import StatusImagesGrid from "../shared/StatusImagesGrid";
 import { Avatar } from "../ui/avatar";
 
-export default function DmMessage({ status }: { status: Status }) {
+interface DmMessageProps {
+  status: Status;
+  align: "left" | "right";
+}
+
+export default function DmMessage({ status, align }: DmMessageProps) {
   const sanitizedHTMLText = DOMPurify.sanitize(status.text);
 
+  const alignClass = align === "left" ? "items-start" : "items-end";
+  const textColorClass = align === "left" ? "text-gray-900" : "text-white";
+  const bgColorClass = align === "right" ? "bg-gray-900" : "bg-gray-200";
+
   return (
-    <div role="status" key="1" className="m-2 border-b status-container">
-      <div className="md:flex">
-        <div className="py-4 w-full">
-          <div className="flex justify-between w-full">
-            <div className="flex">
-              <Avatar className="mr-3">
-                <AvatarImage src={status.avatar} alt={status.name} />
-                <AvatarFallback>NM</AvatarFallback>
-              </Avatar>
-              <div>
-                <div className="uppercase tracking-wide text-sm text-black dark:text-white font-semibold">
-                  {status.name}
-                </div>
-                <div className="text-gray-400 dark:text-gray-300">
-                  <a href={status.authorUrl}>{status.authorUrl}</a>
-                </div>
-              </div>
-            </div>
-            <div role="date" className="text-gray-400 dark:text-gray-300">
-              <RelativeDate date={status.createdAt} />
-            </div>
-          </div>
-
-          <div className="status-content mt-4 text-black dark:text-gray-300">
-            <div className="text-content">
-              <div dangerouslySetInnerHTML={{ __html: sanitizedHTMLText }} />
-            </div>
-
-            {status.images && status.images.length > 0 && (
-              <StatusImagesGrid statusId={status.id} images={status.images} />
-            )}
-          </div>
-          <div className="flex mt-6 justify-between items-center">
-            <div className="flex space-x-4 text-gray-400 dark:text-gray-300 justify-left w-full">
-              <Like
-                objectId={status.id}
-                count={status.likesCount}
-                fillColor="fill-red-500 text-red-500"
-                alreadyActed={status.favourited}
-              />
-              <Bookmark
-                objectId={status.id}
-                active={false}
-                fillColor="text-blue-500 fill-blue-500"
-                alreadyActed={status.bookmarked}
-              />
-            </div>
-          </div>
+    <div className={"flex flex-col " + alignClass}>
+      <div className="flex items-center space-x-2">
+        <div className={"text-sm text-gray-900"}>
+          {status.name} • <RelativeDate date={status.createdAt} />
         </div>
+      </div>
+      <div className={"p-2 rounded-lg " + bgColorClass + " " + textColorClass}>
+        <div dangerouslySetInnerHTML={{ __html: sanitizedHTMLText }} />
+        {status.images && status.images.length > 0 && (
+          <StatusImagesGrid statusId={status.id} images={status.images} />
+        )}
       </div>
     </div>
   );

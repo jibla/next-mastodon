66c40c9de5309b16001a75f64b6c4187f6f7bcdd
Giorgi Jibladze <giorgi@omedia.ge>
2024-01-30 17:04:39 +0400

    Add mobile view. refs #70

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,6 +2,7 @@
 
 import SignInForm from "@/components/auth/SignInForm";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function LoginPage() {

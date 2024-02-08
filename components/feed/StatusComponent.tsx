2166f3987f5a1561d61e37811faf1e5f1db4755c
Giorgi Jibladze <giorgi@omedia.ge>
2024-02-08 15:35:55 +0400

    Show reblogged content. refs #78

--- components/feed/StatusComponent.tsx
+++ components/feed/StatusComponent.tsx
@@ -9,31 +9,57 @@ import Comment from "./actions/Comment";
 import CopyLink from "./actions/CopyLink";
 import Like from "./actions/Like";
 import Share from "./actions/Share";
+import ShareIcon from "../shared/shareIcon";
 
 export default function StatusComponent({ status }: { status: Status }) {
-  const sanitizedHTMLText = DOMPurify.sanitize(status.text);
+  let statusToRender = status;
+  if (status.reblogged) {
+    statusToRender = status.reblogged;
+  }
+
+  const sanitizedHTMLText = DOMPurify.sanitize(statusToRender.text);
 
   return (
     <div role="status" key="1" className="m-2 border-b status-container">
+      {status.reblogged && (
+        <div className="flex items-center">
+          <span className="mr-2">
+            {" reblogged by "}
+            <a
+              href={status.authorUrl}
+              className="text-blue-500 hover:underline"
+            >
+              {status.name}
+            </a>
+          </span>
+
+          <ShareIcon iconClasses="" />
+        </div>
+      )}
       <div className="md:flex">
         <div className="py-4 w-full">
           <div className="flex justify-between w-full">
             <div className="flex">
               <Avatar className="mr-3">
-                <AvatarImage src={status.avatar} alt={status.name} />
+                <AvatarImage
+                  src={statusToRender.avatar}
+                  alt={statusToRender.name}
+                />
                 <AvatarFallback>NM</AvatarFallback>
               </Avatar>
               <div>
                 <div className="uppercase tracking-wide text-sm text-black dark:text-white font-semibold">
-                  {status.name}
+                  {statusToRender.name}
                 </div>
                 <div className="text-gray-400 dark:text-gray-300">
-                  <a href={status.authorUrl}>{status.authorUrl}</a>
+                  <a href={statusToRender.authorUrl}>
+                    {statusToRender.authorUrl}
+                  </a>
                 </div>
               </div>
             </div>
             <div role="date" className="text-gray-400 dark:text-gray-300">
-              <RelativeDate date={status.createdAt} />
+              <RelativeDate date={statusToRender.createdAt} />
             </div>
           </div>
 
@@ -42,36 +68,39 @@ export default function StatusComponent({ status }: { status: Status }) {
               <div dangerouslySetInnerHTML={{ __html: sanitizedHTMLText }} />
             </div>
 
-            {status.images && status.images.length > 0 && (
-              <StatusImagesGrid statusId={status.id} images={status.images} />
+            {statusToRender.images && statusToRender.images.length > 0 && (
+              <StatusImagesGrid
+                statusId={statusToRender.id}
+                images={statusToRender.images}
+              />
             )}
           </div>
           <div className="flex mt-6 justify-between items-center">
             <div className="flex space-x-4 text-gray-400 dark:text-gray-300 justify-center w-full">
               <Like
-                objectId={status.id}
-                count={status.likesCount}
+                objectId={statusToRender.id}
+                count={statusToRender.likesCount}
                 fillColor="fill-red-500 text-red-500"
                 alreadyActed={status.favourited}
               />
               <Comment
-                objectId={status.id}
-                count={status.commentsCount}
+                objectId={statusToRender.id}
+                count={statusToRender.commentsCount}
                 fillColor="text-red-500"
               />
               <Share
-                objectId={status.id}
-                count={status.sharesCount}
+                objectId={statusToRender.id}
+                count={statusToRender.sharesCount}
                 fillColor="text-red-500"
-                alreadyActed={status.shared}
+                alreadyActed={statusToRender.shared}
               />
               <Bookmark
-                objectId={status.id}
+                objectId={statusToRender.id}
                 active={false}
                 fillColor="text-blue-500 fill-blue-500"
-                alreadyActed={status.bookmarked}
+                alreadyActed={statusToRender.bookmarked}
               />
-              <CopyLink objectId={status.id} fillColor="text-red-500" />
+              <CopyLink objectId={statusToRender.id} fillColor="text-red-500" />
             </div>
           </div>
         </div>
--- components/feed/actions/Share.tsx
+++ components/feed/actions/Share.tsx
@@ -1,6 +1,7 @@
 import { actionTypesEnum } from "@/lib/data/core/entities/Actions";
 import { ActionProps } from "./action-props";
 import ActionWrapper from "./ActionWrapper";
+import ShareIcon from "@/components/shared/shareIcon";
 
 interface ShareActionProps extends ActionProps {
   count: number;
@@ -21,23 +22,7 @@ export default function Share({
         fillColor={fillColor}
         actionType={actionTypesEnum.REPOST}
       >
-        <svg
-          className={iconClasses}
-          fill="none"
-          height="24"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          viewBox="0 0 24 24"
-          width="24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="m17 2 4 4-4 4" />
-          <path d="M3 11v-1a4 4 0 0 1 4-4h14" />
-          <path d="m7 22-4-4 4-4" />
-          <path d="M21 13v1a4 4 0 0 1-4 4H3" />
-        </svg>
+        <ShareIcon iconClasses={iconClasses} />
         <span className="ml-1 text-black">{count}</span>
       </ActionWrapper>
     </>

0221dea9b4076df2d457a58653d11a2a467879bf
Giorgi Jibladze <giorgi@omedia.ge>
2024-02-21 14:28:48 +0400

    Change ReplyToDm usecase to ReplyToStatus. refs #90

--- lib/shared/ioc.ts
+++ lib/shared/ioc.ts
@@ -20,7 +20,7 @@ import DirectMessagesPortInMemoryAdapter from "../data/adapters/direct-messages/
 import { ListDirectMessagesUseCase } from "../data/core/use-cases/list-direct-messages/ListDirectMessagesUseCase";
 import { DirectMessagesPortMastojsAdapter } from "../data/adapters/direct-messages/DirectMessagesPortMastojsAdapter";
 import { ReadConversationUseCase } from "../data/core/use-cases/read-conversation/ReadConversationUseCase";
-import { ReplyToDmUseCase } from "../data/core/use-cases/reply-to-dm/ReplyToDmUseCase";
+import { ReplyToStatusUseCase } from "../data/core/use-cases/reply-to-status/ReplyToStatusUseCase";
 import { FetchStatusThreadUseCase } from "../data/core/use-cases/fetch-status-thread/FetchStatusThreadUseCase";
 
 const container = new Container();
@@ -33,7 +33,7 @@ container.bind<UseCase>("actions").to(ActionsUseCase);
 container.bind<UseCase>("publish-status").to(PublishStatusUseCase);
 container.bind<UseCase>("list-direct-messages").to(ListDirectMessagesUseCase);
 container.bind<UseCase>("read-conversation").to(ReadConversationUseCase);
-container.bind<UseCase>("reply-to-dm").to(ReplyToDmUseCase);
+container.bind<UseCase>("reply-to-status").to(ReplyToStatusUseCase);
 container.bind<UseCase>("fetch-status-thread").to(FetchStatusThreadUseCase);
 
 // adapters
